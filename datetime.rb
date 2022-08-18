98a8c6940e0c2f6e8e9079051d7aa818709647a1
LuisBanderasB <luis.banderas@enroutesystems.com>
2022-08-18 10:21:24 -0500

    agoust-18

---
+++ datetime.rb
@@ -0,0 +1,13 @@
+# 3 active support methods en date and time
+# strftime on a date
+
+Time.now.next_day(1)
+Time.current
+Time.now.prev_day(100)
+
+Time.now.strftime("Today is %A")
+Time.now.strftime("%d-%m-%Y at %I:%M %p")
+
+# ----------TEAM----------
+# Fancisco Luna
+# Luis Banderas
